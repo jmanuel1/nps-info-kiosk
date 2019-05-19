ffdd333648dc20c46f68d5d5cd85f475633144c7
Jason Manuel <jama.indo@hotmail.com>
2019-05-19 11:02:27 -0700

    Create SearchBarComponent
    This component will act as a search bar linked to a search service.

--- sails-app/assets/app/app/app.module.ts
+++ sails-app/assets/app/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,16 +8,19 @@ import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 // TODO: Add autocomplete later
 import {MatFormFieldModule} from '@angular/material';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
