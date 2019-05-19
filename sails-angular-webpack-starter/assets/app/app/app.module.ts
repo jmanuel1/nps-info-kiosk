dca219df9c2c98589ffeea8f0036b483d8071c5a
Jason Manuel <jama.indo@hotmail.com>
2019-05-19 00:13:10 -0700

    Set up Angular Material
    Angular Material has been installed into the project and will be used
    for the UI.

--- sails-angular-webpack-starter/assets/app/app/app.module.ts
+++ sails-angular-webpack-starter/assets/app/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+// TODO: Add autocomplete later
+import {MatFormFieldModule} from '@angular/material';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
