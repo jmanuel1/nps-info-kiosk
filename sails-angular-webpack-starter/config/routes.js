8a7ca464da99b52482c88691bc6c782d23b8536e
Jason Manuel <jama.indo@hotmail.com>
2019-05-18 21:27:38 -0700

    Comment out all unused Sails routes

--- sails-angular-webpack-starter/config/routes.js
+++ sails-angular-webpack-starter/config/routes.js
@@ -14,7 +14,7 @@ module.exports.routes = {
   //  ║║║║╣ ╠╩╗╠═╝╠═╣║ ╦║╣ ╚═╗
   //  ╚╩╝╚═╝╚═╝╩  ╩ ╩╚═╝╚═╝╚═╝
   'GET /':                   { action: 'assets/get-static-asset' },
-  'GET /welcome/:unused?':   { action: 'dashboard/view-welcome' },
+  // 'GET /welcome/:unused?':   { action: 'dashboard/view-welcome' },
 
   // 'GET /faq':                { action:   'view-faq' },
   // 'GET /legal/terms':        { action:   'legal/view-terms' },
@@ -39,7 +39,7 @@ module.exports.routes = {
   //  ╔╦╗╦╔═╗╔═╗  ╦═╗╔═╗╔╦╗╦╦═╗╔═╗╔═╗╔╦╗╔═╗   ┬   ╔╦╗╔═╗╦ ╦╔╗╔╦  ╔═╗╔═╗╔╦╗╔═╗
   //  ║║║║╚═╗║    ╠╦╝║╣  ║║║╠╦╝║╣ ║   ║ ╚═╗  ┌┼─   ║║║ ║║║║║║║║  ║ ║╠═╣ ║║╚═╗
   //  ╩ ╩╩╚═╝╚═╝  ╩╚═╚═╝═╩╝╩╩╚═╚═╝╚═╝ ╩ ╚═╝  └┘   ═╩╝╚═╝╚╩╝╝╚╝╩═╝╚═╝╩ ╩═╩╝╚═╝
-  '/terms':                   '/legal/terms',
+  // '/terms':                   '/legal/terms',
   // '/logout':                  '/api/v1/account/logout',
 
 
@@ -54,14 +54,14 @@ module.exports.routes = {
   //  ╩ ╩╩  ╩  ╚═╝╝╚╝═╩╝╩  ╚═╝╩╝╚╝ ╩ ╚═╝
   // Note that, in this app, these API endpoints may be accessed using the `Cloud.*()` methods
   // from the Parasails library, or by using those method names as the `action` in <ajax-form>.
-  '/api/v1/account/logout':                           { action: 'account/logout' },
-  'PUT   /api/v1/account/update-password':            { action: 'account/update-password' },
-  'PUT   /api/v1/account/update-profile':             { action: 'account/update-profile' },
-  'PUT   /api/v1/account/update-billing-card':        { action: 'account/update-billing-card' },
-  'PUT   /api/v1/entrance/login':                        { action: 'entrance/login' },
-  'POST  /api/v1/entrance/signup':                       { action: 'entrance/signup' },
-  'POST  /api/v1/entrance/send-password-recovery-email': { action: 'entrance/send-password-recovery-email' },
-  'POST  /api/v1/entrance/update-password-and-login':    { action: 'entrance/update-password-and-login' },
-  'POST  /api/v1/deliver-contact-form-message':          { action: 'deliver-contact-form-message' },
+  // '/api/v1/account/logout':                           { action: 'account/logout' },
+  // 'PUT   /api/v1/account/update-password':            { action: 'account/update-password' },
+  // 'PUT   /api/v1/account/update-profile':             { action: 'account/update-profile' },
+  // 'PUT   /api/v1/account/update-billing-card':        { action: 'account/update-billing-card' },
+  // 'PUT   /api/v1/entrance/login':                        { action: 'entrance/login' },
+  // 'POST  /api/v1/entrance/signup':                       { action: 'entrance/signup' },
+  // 'POST  /api/v1/entrance/send-password-recovery-email': { action: 'entrance/send-password-recovery-email' },
+  // 'POST  /api/v1/entrance/update-password-and-login':    { action: 'entrance/update-password-and-login' },
+  // 'POST  /api/v1/deliver-contact-form-message':          { action: 'deliver-contact-form-message' },
 
 };
