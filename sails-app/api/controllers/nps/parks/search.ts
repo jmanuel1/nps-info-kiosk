a90e4624c45217ca9bd62cbe1727392eefec6238
Jason Manuel <jama.indo@hotmail.com>
2019-05-20 17:57:01 -0700

    Add support for state and designation filtering on server
    Partly satisfies core deliverable requirement 1a, part 2.

--- sails-app/api/controllers/nps/parks/search.ts
+++ sails-app/api/controllers/nps/parks/search.ts
@@ -3,24 +3,76 @@ import { stringify } from 'querystring';
 
 declare var sails: any;
 
+class Parameters {
+  term: string;
+  state: string;
+  designation: string;
+}
+
+class NPSParameters {
+  parkCode: string;
+  stateCode: string;
+
+  static fromOurParameters(parameters: Parameters) {
+    return {
+      parkCode: parameters.term,
+      stateCode: parameters.state
+    };
+  }
+}
+
 /* NOTE and shrug: For some unknown reason, using a machine action doesn't work
 /* because something else finishes the request early. */
 module.exports = async function search(req, res) {
-    const NPS_API_KEY = getNPSAPIKey();
-    const term: string = req.param('term');
+    let NPS_API_KEY;
+    try {
+      NPS_API_KEY = getNPSAPIKey();
+    } catch (e) {
+      sails.log.error('No API key for the NPS Data API!');
+      sails.log.error('Make sure to set an environment variable NPS_API_KEY=<your API key>.');
+      return res.serverError();
+    }
+    const parameters = getParameters(req);
     // For now, just make the park codes searchable
     const npsResponse =
-      await makeNPSRequest('GET', '/parks', { parkCode: term }, NPS_API_KEY);
-    console.log(res.getHeaders());
-    res.status(200).write(JSON.stringify({ results: npsResponse }));
+      await makeNPSRequest('GET', '/parks', NPSParameters.fromOurParameters(parameters), NPS_API_KEY);
+    const ourResponse = {
+      results: filterByDesignation(npsResponse, parameters.designation)
+    };
+
+    // sails.log.debug(res.getHeaders());
+    res.status(200).write(JSON.stringify(ourResponse));
     return res.end();
   };
 
+function filterByDesignation(npsResponse, designation: string) {
+  const parks = npsResponse.data;
+
+  // Don't filter if no designation has been specified
+  if (!designation) {
+    return npsResponse;
+  }
+
+  // TODO: Don't use exact matching
+  const filteredParks = parks.filter((park) => {
+    return park.designation === designation;
+  });
+
+  return { ...npsResponse, data: filteredParks };
+}
+
+function getParameters(req): Parameters {
+  return {
+    term: req.param('term'),
+    state: req.param('state'),
+    designation: req.param('designation')
+  };
+}
 
 function makeNPSRequest(
   method: string,
   endpoint: string,
-  parameters: object,
+  parameters: NPSParameters,
   apiKey: string): Promise<object> {
   let normalizedEndpoint: string;
   if (endpoint[0] === '/') {
@@ -56,5 +108,11 @@ function makeNPSRequest(
 
 function getNPSAPIKey(): string {
   // Get the API key from an environment variable
-  return process.env.NPS_API_KEY;
+  const key = process.env.NPS_API_KEY;
+
+  if (!key) {
+    throw new Error('No NPS API key');
+  }
+
+  return key;
 }
