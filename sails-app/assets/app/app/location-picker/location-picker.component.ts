f23f3d1838dc06f03ff1138752924915838c7a47
Jason Manuel <jama.indo@hotmail.com>
2019-05-27 19:15:04 -0700

    Close the location picker map on mouseout

--- sails-app/assets/app/app/location-picker/location-picker.component.ts
+++ sails-app/assets/app/app/location-picker/location-picker.component.ts
@@ -1,28 +1,26 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { point, Coord, getCoord } from '@turf/turf';
 import { LocationService } from './location.service';
 
-declare let L; // from npmap.js/leaflet
+declare let L: any; // from npmap.js/leaflet
 declare global {
   interface Window {
-    NPMap;
+    NPMap: any;
   }
 }
 
 @Component({
   selector: 'app-location-picker',
   templateUrl: './location-picker.component.html',
-  styleUrls: ['./location-picker.component.css'],
-  providers: [LocationService]
+  styleUrls: ['./location-picker.component.css']
 })
 export class LocationPickerComponent implements OnInit {
 
   isMapOpen: boolean;
   location: Coord;
-  marker;
-  // @Output() locationchange = new EventEmitter<Coord>();
+  marker: any; // TODO: leaflet typings?
 
-  constructor(private locationService: LocationService) {
+  constructor(private zone: NgZone) {
 
   }
 
@@ -30,9 +28,10 @@ export class LocationPickerComponent implements OnInit {
     window.NPMap = {
       div: 'map',
       hooks: {
-        init: (callback) => {
+        init: (callback: () => void) => {
           this.marker = L.marker([0, 0]).addTo(window.NPMap.config.L);
-          window.NPMap.config.L.on('click', (e) => this.setLocation(e));
+          window.NPMap.config.L.on('click', (e: any) => this.setLocation(e));
+          window.NPMap.config.L.on('mouseout', () => this.closeMap());
           callback();
         }
       }
@@ -40,23 +39,23 @@ export class LocationPickerComponent implements OnInit {
     const s = document.createElement('script');
     s.src = 'https://www.nps.gov/lib/npmap.js/4.0.0/npmap-bootstrap.js';
     document.body.appendChild(s);
-    // s.addEventListener('load', (event) => {
-    //   this.marker = L.marker([0, 0]).addTo(NPMap.config.L);
-    // });
-
   }
 
   toggleMap() {
     this.isMapOpen = !this.isMapOpen;
   }
 
-  setLocation(event) {
+  closeMap() {
+    this.isMapOpen = false;
+    // force change detection
+    this.zone.run(() => null);
+  }
+
+  setLocation(event: any) {
     console.dir(event);
     this.location = point([event.latlng.lat, event.latlng.lng]);
     this.updateMarker(this.location);
     LocationService.nextLocation(this.location);
-    // console.debug('location-picker emitted: ');
-    // console.dir(this.location);
   }
 
   updateMarker(location: Coord) {
