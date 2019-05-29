83699e1570ee6b0f711483be488710a3ebcd85d7
Jason Manuel <jama.indo@hotmail.com>
2019-05-28 17:35:59 -0700

    Return correct content type for css

--- sails-app/api/controllers/assets/get-static-asset.ts
+++ sails-app/api/controllers/assets/get-static-asset.ts
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+import * as fs from 'fs';
+import * as path from 'path';
 
 /* Gets a static asset from assets/dist. */
 module.exports = {
@@ -20,25 +20,32 @@ module.exports = {
   },
 
 
-  fn: async function(inputs) {
+  async fn() {
 
     const pth = isURLPathEmpty(this.req.path) ? 'index.html' : this.req.path;
     // Current working directory is root of sails project
     const pathToFile = path.join('assets/dist', pth);
-    let fileContents;
+    let fileContents: string;
     try {
       fileContents = fs.readFileSync(pathToFile, 'utf8');
     } catch (err) {
       // To be compatible with the client router, just get index.html
-      fileContents = fs.readFileSync('assets/dist/index.html');
+      fileContents = fs.readFileSync('assets/dist/index.html', 'utf8');
     }
-    return this.res.status(200).set('Content-Type', 'text/html').send(fileContents);
+    return this.res.status(200).set('Content-Type', getContentType(pth))
+      .send(fileContents);
 
   }
 
 
 };
 
-function isURLPathEmpty(pth) {
+function getContentType(pth: string) {
+  const extension = path.extname(pth);
+  const typeMap = { '.css': 'text/css' };
+  return typeMap[extension] || 'text/html';
+}
+
+function isURLPathEmpty(pth: string) {
   return !pth || pth === '' || pth === '/';
 }
