ed3c7fd0a9e3d5ed96e69232174881aa7af1f288
Jason Manuel <jama.indo@hotmail.com>
2019-05-29 00:26:13 -0700

    Disable Angular router tracing

--- sails-app/assets/app/app/app-routing.module.ts
+++ sails-app/assets/app/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { SearchPageComponent } from './search-page/search-page.component';
 import { ParkPageComponent } from './park-page/park-page.component';
 
+// NOTE: Set to true only during development
+const ENABLE_ROUTER_TRACING = false;
+
 const routes: Routes = [
   {
     path: 'search',
@@ -24,7 +27,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {enableTracing: true})],
+  imports: [RouterModule.forRoot(
+    routes,
+    {enableTracing: ENABLE_ROUTER_TRACING}
+  )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
