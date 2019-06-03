76e23ff892eb1b818eeb133e087f8d69152f3852
Jason Manuel <jama.indo@hotmail.com>
2019-06-02 19:55:41 -0700

    Fix search page title

--- sails-app/assets/app/app/search-page/search-page.component.ts
+++ sails-app/assets/app/app/search-page/search-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-search-page',
@@ -6,11 +7,14 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./search-page.component.css']
 })
 export class SearchPageComponent implements OnInit {
-  @Input() title: string;
+  title: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.data.subscribe((data) => {
+      this.title = data.title;
+    });
   }
 
 }
